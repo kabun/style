84ed7c48a1565d31cdb63a938b0f296e9ae55ac2
kabun <cococodm@gmail.com>
2018-05-07 01:14:15 +0900

    base

---
+++ develop/js/common.js
@@ -0,0 +1,19 @@
+$(function(){
+  // 初期化
+
+
+  // ページ内リンクスクロール
+  $("a[href^='#']").click(function() {
+     var speed = 200;
+     var href= $(this).attr("href");
+     var target = $(href == "#" || href == "" ? 'html' : href);
+     var position = target.offset().top - 10;
+     $('body,html').animate({scrollTop:position}, speed, 'swing');
+     return false;
+  });
+
+
+
+
+
+});
